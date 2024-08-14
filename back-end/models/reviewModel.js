7c1b3da5616d6fecf95207ae8f310af771b4a6fe
xhuve <xhuvelialbana@gmail.com>
2024-08-14 12:18:28 +0200

    add backend database functionality, add basic models

---
+++ back-end/models/reviewModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+export const reviewSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+
---
+++ back-end/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
+dotenv.config()
+import { connectDB } from './config/db.js'
+
+const PORT = 5000
+connectDB()
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product);
+})
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
